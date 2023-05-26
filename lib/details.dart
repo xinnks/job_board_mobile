5aa0b1c7c51d20f984f415aa8238367a0782e03e
James Sinkala <xinnks@gmail.com>
2023-05-26 15:19:20 +0300

    feat: Add details page

---
+++ lib/details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class DetailsPage extends StatefulWidget {
+  const DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  DetailsPageState createState() => DetailsPageState();
+}
+
+class DetailsPageState extends State<DetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
