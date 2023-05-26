bcc1e30ce121a804c2119c8944c2b174b473b318
James Sinkala <xinnks@gmail.com>
2023-05-26 15:19:54 +0300

    feat: Add job listing page

---
+++ lib/list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ListPage extends StatefulWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  ListPageState createState() => ListPageState();
+}
+
+class ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
